bb309a17e5a0fd82839610536a53f7b774b9e697
invo <alex_ilioi@yahoo.com>
2020-06-09 11:40:19 +0300

    added vertex and fragment shader and linked them into a programme

--- Robotel/main.cpp
+++ Robotel/main.cpp
@@ -7,9 +7,12 @@ using namespace std;
 
 float* vertices;
 unsigned int VBO;
+unsigned int shaderProgram;
+
+#pragma region Load din fisiere
 
 //in document nu pui f sau ,
-void initializeVertexArray(string path) {
+void initializeVertexArrayFromFile(string path) {
 	long long m,n;
 	ifstream f(path);
 	
@@ -30,10 +33,84 @@ void initializeVertexArray(string path) {
 		}
 		cout << endl;
 	}
+	f.close();
 }
+
+char* loadShaderFromFile(string path)
+{
+	string text;
+	string line;
+	ifstream f(path);
+	while (getline(f, line)) {
+		text += line + '\n';
+	}
+	f.close();
+	char* tmp = new char[text.size() + 1];
+	copy(text.begin(), text.end(), tmp);
+	tmp[text.size()] = '\0';
+	return tmp;
+}
+#pragma endregion
+
 void initBuffers() 
 {
 	glGenBuffers(1, &VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+void initShaders()
+{
+	//shaders
+	unsigned int vertexShader;
+	unsigned int fragmentShader;
+	//error handling
+	int  success;
+	char infoLog[512];
+	
+	//initializeaza vertex shader
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	const char* vertexShaderSource = loadShaderFromFile("vertex.vert");
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	
+	//nu uita sa dealoci
+	delete[] vertexShaderSource;
+	
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	//initializeaza fragment shader
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	const char* fragmentShaderSource = loadShaderFromFile("fragment.frag");
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	//nu uita sa dealoci
+	delete[] fragmentShaderSource;
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		cout << "EROARE LA LINK" << endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 }
 #pragma region GLFW Input & Callbacks
 
@@ -79,13 +156,14 @@ int main()
 	///////////////////////////////////////
 	//AICI INITIALIZAM ARRAY-UL DE VERTICES
 	///////////////////////////////////////
-	initializeVertexArray("triunghi.txt");
+	initializeVertexArrayFromFile("triunghi.txt");
 	
 	///////////////////////////////////////
 	//AICI INITIALIZAM BUFFERELE///////////
 	///////////////////////////////////////
 	initBuffers();
-
+	initShaders();
+	glUseProgram(shaderProgram);
 	//keep the window open
 	while (!glfwWindowShouldClose(window))
 	{
