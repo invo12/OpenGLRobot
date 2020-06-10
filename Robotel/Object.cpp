7dd56a69e2acfda48338ad9d0322f5471e7fbf6c
murinho99 <56977820+murinho99@users.noreply.github.com>
2020-06-10 19:45:21 +0300

    Read Material data from .mtl files

--- Robotel/Object.cpp
+++ Robotel/Object.cpp
@@ -1,10 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "Object.h"
 
 
 Object::Object(std::string numeFisier)
 {
     Assimp::Importer importer;
-    const aiScene* modelScene = importer.ReadFile(numeFisier, aiProcess_MakeLeftHanded | aiProcess_FlipWindingOrder | aiProcess_FlipUVs | aiProcess_PreTransformVertices |
+    aiMesh* mesh;
+    std::string importModelPath = numeFisier + ".obj";
+    const aiScene* modelScene = importer.ReadFile(importModelPath, aiProcess_MakeLeftHanded | aiProcess_FlipWindingOrder | aiProcess_FlipUVs | aiProcess_PreTransformVertices |
         aiProcess_CalcTangentSpace |
         aiProcess_GenSmoothNormals |
         aiProcess_Triangulate |
@@ -14,43 +17,107 @@ Object::Object(std::string numeFisier)
         );
     if (modelScene)
     {
-        this->modelMesh = modelScene->mMeshes[0];
-        std::cout << "a";
+        mesh = modelScene->mMeshes[0];
+        aiFace* face;
+        for (unsigned int v = 0; v < mesh->mNumVertices; v++)
+        {
+            vertexBuff.push_back(mesh->mVertices[v].x);
+            vertexBuff.push_back(mesh->mVertices[v].y);
+            vertexBuff.push_back(mesh->mVertices[v].z);
+
+            vertexBuff.push_back(mesh->mNormals[v].x);
+            vertexBuff.push_back(mesh->mNormals[v].y);
+            vertexBuff.push_back(mesh->mNormals[v].z);
+
+            if (mesh->HasTextureCoords(0)) {
+                vertexBuff.push_back(mesh->mTextureCoords[0][v].x);
+                vertexBuff.push_back(mesh->mTextureCoords[0][v].y);
+            }
+            else
+            {
+                vertexBuff.push_back(0);
+                vertexBuff.push_back(0);
+            }
+        }
+        for (unsigned int f = 0; f < mesh->mNumFaces; f++)
+        {
+            face = &mesh->mFaces[f];
+            indexBuff.push_back(face->mIndices[0]);
+            indexBuff.push_back(face->mIndices[1]);
+            indexBuff.push_back(face->mIndices[2]);
+        }
     }
-    assimpGetMeshData(this->modelMesh);
+
+    std::string importMaterialPath = numeFisier + ".mtl";
+    FILE* file = fopen(importMaterialPath.c_str(), "r");
+    if (file == NULL) {
+        printf("Impossible to open material!\n");
+    }
+    else {
+        while (1) {
+            char lineHeader[128];
+            int res = fscanf(file, "%s", lineHeader);
+            if (res == EOF) { break; }
+            if (strcmp(lineHeader, "Ka") == 0) {
+                glm::vec3 ambient;
+                fscanf(file, "%f %f %f\n", &ambient.x, &ambient.y, &ambient.z);
+                this->materialData.ambient = ambient;
+            }
+            else if (strcmp(lineHeader, "Kd") == 0) {
+                glm::vec3 diffuse;
+                fscanf(file, "%f %f %f\n", &diffuse.x, &diffuse.y, &diffuse.z);
+                this->materialData.diffuse = diffuse;
+            } if (strcmp(lineHeader, "Ks") == 0) {
+                glm::vec3 specular;
+                fscanf(file, "%f %f %f\n", &specular.x, &specular.y, &specular.z);
+                this->materialData.specular = specular;
+            } if (strcmp(lineHeader, "Ns") == 0) {
+                float exponent;
+                fscanf(file, "%f\n", &exponent);
+                this->materialData.specularExponent = exponent;
+            }if (strcmp(lineHeader, "d") == 0) {
+                float dissolved;
+                fscanf(file, "%f\n", &dissolved);
+                this->materialData.dissolved = dissolved;
+            }if (strcmp(lineHeader, "Ni") == 0) {
+                float density;
+                fscanf(file, "%f\n", &density);
+                this->materialData.density = density;
+            }
+        }
+    }
 }
 
-bool Object::assimpGetMeshData(const aiMesh* mesh)
+void Object::ResetModelMatrix()
 {
-    aiFace* face;
-    for (unsigned int v = 0; v < mesh->mNumVertices; v++)
-    {
-        vertexBuff.push_back(mesh->mVertices[v].x);
-        vertexBuff.push_back(mesh->mVertices[v].y);
-        vertexBuff.push_back(mesh->mVertices[v].z);
+    this->model = glm::mat4(1.0f);
+}
 
-        vertexBuff.push_back(mesh->mNormals[v].x);
-        vertexBuff.push_back(mesh->mNormals[v].y);
-        vertexBuff.push_back(mesh->mNormals[v].z);
+glm::mat4 Object::GetModelMatrix()
+{
+    return this->model;
+}
 
-        if (mesh->HasTextureCoords(0)) {
-            vertexBuff.push_back(mesh->mTextureCoords[0][v].x);
-            vertexBuff.push_back(mesh->mTextureCoords[0][v].y);
-        }
-        else
-        {
-            vertexBuff.push_back(0);
-            vertexBuff.push_back(0);
-        }
-    }
-    for (unsigned int f = 0; f < mesh->mNumFaces; f++)
-    {
-        face = &mesh->mFaces[f];
-        indexBuff.push_back(face->mIndices[0]);
-        indexBuff.push_back(face->mIndices[1]);
-        indexBuff.push_back(face->mIndices[2]);
-    }
-    return true;
+void Object::Translate(glm::vec3 translate)
+{
+    this->model = glm::translate(this->model, glm::vec3(translate.x, translate.y, translate.z));
+}
+
+void Object::Rotate(float angle1, glm::vec3 rotationAxis)
+{
+    this->model = glm::rotate(this->model, angle1, rotationAxis);
+}
+
+void Object::Scale(glm::vec3 scale)
+{
+    this->model = glm::scale(this->model, scale);
+}
+
+Object::Object(const Object& object)
+{
+    this->vertexBuff = object.vertexBuff;
+    this->indexBuff = object.indexBuff;
+    this->model = object.model;
 }
 
 
@@ -62,4 +129,5 @@ std::vector<float> Object::GetVertexBuffer()
 std::vector<unsigned int> Object::GetIndexBuffer()
 {
     return this->indexBuff;
+
 }
--- Robotel/Object.h
+++ Robotel/Object.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <fstream>
 #include <iostream>
 #include <assimp/Importer.hpp>
@@ -9,31 +10,39 @@
 #include <assimp/postprocess.h>
 #include <assimp/matrix4x4.h>
 #include <assimp/cimport.h>
-
+struct Material {
+	glm::vec3 ambient;
+	glm::vec3 diffuse;
+	glm::vec3 specular;
+	float specularExponent;
+	float dissolved;
+	float density;
+};
 class Object
 {
 private:
-	const aiMesh* modelMesh;
+	Material materialData;
 	glm::mat4 model = glm::mat4(1.0f);
-
 	std::vector<float> vertexBuff;
 	std::vector<unsigned int>indexBuff;
-
 
 public:
 	Object(std::string numeFisier);
 	std::vector<float> GetVertexBuffer();
 	std::vector<unsigned int>GetIndexBuffer();
-	bool assimpGetMeshData(const aiMesh* mesh);
-	//model matrix
-	//get model matrix
-	//Object GetModel();
-	//Translate
-	//Rotate
-	//Scale
-	//Fisier Texturi
+	void ResetModelMatrix();
+	glm::mat4 GetModelMatrix();
+	void Translate(glm::vec3 translate);
+	void Rotate(float angle1,glm::vec3 rotationAxis);
+	void Scale(glm::vec3 scale);
+	Object(const Object& object);
+	//TODO CITIRE MATERIAL + STRUCTURA STOCARE TOT IN OBJECT
+
+	//TODO CLONE!
+	//Fisier Texturi -> TODO CLASA TEXTURI
+	//TODO ADAUGARE SHADER IN OBIECT
 	//Fisier Normala
-	//Draw
+	//Draw()
 
 };
 
